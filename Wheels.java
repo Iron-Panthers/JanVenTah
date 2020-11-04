b80e8ea894158065b96f22e3699c1ba617813d3e
Mira Venugopal <23venugopalm75@stu.smuhsd.org>
2020-11-03 17:57:48 -0800

    Create Wheels Class

---
+++ Wheels.java
@@ -0,0 +1,12 @@
+package JanVenTah;
+
+class Wheels 
+{
+    private double wheelSize;
+    private int numTires;
+    public Wheels(double wheelSize, int numTires)
+    {
+        this.wheelSize = heelSize;
+        this.numTires = numTires;
+    }
+}
