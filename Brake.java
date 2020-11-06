75bac6534429f26e14dadb8ce47936332b475e4b
Mira Venugopal <23venugopalm75@stu.smuhsd.org>
2020-11-05 18:19:32 -0800

    Creation of Brake.java

--- Brake.java
+++ Brake.java
@@ -1,11 +1,12 @@
-package JanVenTeh; 
-class Brake {
-private int pads;
-private double wheel cylinders;
-}
-class Brake {
-int pads= 4;
-}
-this.pads=pads;
-this.wheel cylinders=wheel cylinders
+package JanVenTeh;
 
+public class Brake 
+{
+    private int pads;
+    private double wheelCylinders;
+    public Brake()
+    {
+        pads = 4;
+    }
+
+}
