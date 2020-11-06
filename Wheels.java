221f0023fd6b90d8edb70aa793ae7d816c14a011
Mira Venugopal <23venugopalm75@stu.smuhsd.org>
2020-11-05 18:08:11 -0800

    Comments on code
    For better understanding

--- Wheels.java
+++ Wheels.java
@@ -1,15 +1,15 @@
 package JanVenTah;
 
-class Wheels 
+class Wheels //start Wheels class
 {
     private double wheelSize;
     private int numTires;
-    public Wheels(double wheelSize, int numTires)
+    public Wheels(double wheelSize, int numTires)//multi-arg constructor
     {
         this.wheelSize = wheelSize;
         this.numTires = numTires;
     }
-    public Wheels()
+    public Wheels()//zero-arg constructor
     {
         double wheelSize = 19.6;
         int numTires = 2;
