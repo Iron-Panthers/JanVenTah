7636bba6211f3d6e8b2427b77a9dff0cfd21358f
Katherine Jankowski <23jankowskik86@stu.smuhsd.org>
2020-11-10 18:25:35 -0800

    stuff happened
    bugs have been fixed by an unnamed person

--- BikeProject/Gears.java
+++ BikeProject/Gears.java
@@ -1,1 +1,16 @@
-public class Gears
+public class Gears {
+
+Package JanVenTah;
+
+private int rings; 
+private boolean frontgears; 
+public Gears() { 
+int rings= 8; 
+boolean frontgears = true;
+} public Gears (int rings, boolean frontgears) {
+    this.rings=rings;
+    this.frontgears=frontgears; 
+
+}
+ 
+}
